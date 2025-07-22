b8e18f2057d113047984b1007d4921341d4f4d2e
alim7007 <olimtoni7007@gmail.com>
2025-07-22 22:22:30 +0300

    update: Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ![ScreenShot](Screenshot.png)
+
+
+### Key Commands Used by CI:
+
+```bash
+terraform -chdir=${{ matrix.path }} fmt -check -recursive
+terraform -chdir=${{ matrix.path }} init
+terraform -chdir=${{ matrix.path }} validate
+terraform -chdir=${{ matrix.path }} plan
+terraform -chdir=${{ matrix.path }} apply -auto-approve
+
+```
--- postgres/docker_entrypoint.sh
+++ postgres/docker_entrypoint.sh
@@ -1,10 +1,10 @@
-# #!/bin/bash
-# set -e
+#!/bin/bash
+set -e
 
-# echo "Custom entrypoint: configuring Postgres..."
+echo "Custom entrypoint: configuring Postgres..."
 
-# # Пример: устанавливаем listen_addresses
-# echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
+# Пример: устанавливаем listen_addresses
+echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
 
-# # Запускаем Postgres от имени пользователя postgres
-# exec su postgres -c "/usr/local/bin/docker-entrypoint.sh postgres"
+# Запускаем Postgres от имени пользователя postgres
+exec su postgres -c "/usr/local/bin/docker-entrypoint.sh postgres"
